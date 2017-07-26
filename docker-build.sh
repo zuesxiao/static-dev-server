37be24d12dbed9942a5fa6a19310be1440221d4e
Xiao Ling <zuesxiao@users.noreply.github.com>
2017-07-26 09:22:23 +0800

    add command to remove unstaged images

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ "$(docker images -q static-dev-server)" -ne "" ]]; then
+if [ "$(docker images | grep "^<none>")" ]; then
+  docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
+fi
+
+if [ "$(docker ps -q -f ancestor=static-dev-server)" ]; then
+  docker rm -f static-dev-server
+fi
+
+if [ "$(docker images -q static-dev-server)" ]; then
   docker rmi -f static-dev-server
 fi
+
 docker build --tag static-dev-server .
