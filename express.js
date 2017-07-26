163373f80814a6205fa69591cfa93313b6d423fe
Xiao Ling <zuesxiao@users.noreply.github.com>
2017-07-26 17:45:58 +0800

    add libs

--- express.js
+++ express.js
@@ -5,11 +5,15 @@
 const path = require('path');
 const express = require('express');
 
-module.exports.init = (publicPath) => {
+module.exports.init = (publicPath, publicPathex) => {
   const app = express();
 
   app.disable('x-powered-by');
+  app.use('/', express.static(path.resolve('workspace/running'), {maxAge: 3600 * 24}));
   app.use('/', express.static(path.resolve(publicPath), {maxAge: 0}));
+  if (publicPathex) {
+    app.use('/', express.static(path.resolve(publicPathex), {maxAge: 3600 * 24}));
+  }
 
   app.use(function (err, req, res, next) {
     if (!err) {
--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const port = process.env.PORT || 3000;
 const watchPath = process.env.WATCH_PATH || 'workspace';
 const watchCmd = process.env.WATHC_CMD || 'npm run build';
 const publicPath = process.env.PUBLIC_PATH || 'workspace/dist';
+const publicpathex = process.env.PUBLIC_PATH_EX;
 
 const watcher = require('./watcher');
 const express = require('./express');
 
-const app = express.init(publicPath);
+const app = express.init(publicPath, publicpathex);
 
 app.listen(port, () => {
   watcher.watch(watchPath, watchCmd);
