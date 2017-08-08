36306d077abab0145c4a178faaecb14925a70468
Xiao Ling <zuesxiao@users.noreply.github.com>
2017-08-08 10:36:57 +0800

    change bash script

--- docker-build.sh
+++ docker-build.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+IMAGE_NAME="static-dev-server"
+CONTAINER_NAME="static-dev-server"
+
 if [ "$(docker images | grep "^<none>")" ]; then
   docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
 fi
 
-if [ "$(docker ps -q -f ancestor=static-dev-server)" ]; then
-  docker rm -f static-dev-server
+if [ "$(docker ps -q -f ancestor=$CONTAINER_NAME)" ]; then
+  docker rm -f $CONTAINER_NAME
 fi
 
-if [ "$(docker images -q static-dev-server)" ]; then
-  docker rmi -f static-dev-server
+if [ "$(docker images -q $IMAGE_NAME)" ]; then
+  docker rmi -f $IMAGE_NAME
 fi
 
 docker build --tag static-dev-server .
--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ "$(docker ps -q -f ancestor=static-dev-server)" -ne "" ]]; then
-  docker rm -f static-dev-server
+IMAGE_NAME="query-service"
+CONTAINER_NAME="query-service"
+
+if [[ "$(docker ps -q -f ancestor=$CONTAINER_NAME)" -ne "" ]]; then
+  docker rm -f $CONTAINER_NAME
 fi
 
 docker run -d \
   -p 3000:3000 \
   --restart=always \
-  --name=static-dev-server \
-  static-dev-server:latest
+  --name=$CONTAINER_NAME \
+  $IMAGE_NAME:latest
